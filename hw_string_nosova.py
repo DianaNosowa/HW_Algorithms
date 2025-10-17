295afe99b6158d7ff3e6908d9ed32c62a5b6dbd1
DianaNosowa <nosowa.di@gmail.com>
2025-10-17 22:48:03 +0300

    Upd hw_string_nosova.py

--- hw_string_nosova.py
+++ hw_string_nosova.py
@@ -1,27 +1,35 @@
 
-def longest_proper_prefix(word):
-    """Returns the longest proper prefix of a string that is also its suffix."""
+def prefix_func(s):
+    """Computes prefix function of the pattern."""
 
-    n = len(word)
-    prefix = [0] * n   # stores lengths of longest prefix-suffix for each position
-    j = 0              # current length of the prefix-suffix match
+    n = len(s)
+    pi = [0] * n  # stores lengths of longest prefix for each position
+    j = 0         # current length of the prefix match
 
     for i in range(1, n):
         # if mismatch -> fallback
-        while j > 0 and word[i] != word[j]:
-            j = prefix[j - 1]
+        while j > 0 and s[i] != s[j]:
+            j = pi[j - 1]
 
-        # if match -> extend the current prefix-suffix
-        if word[i] == word[j]:
+        # if match -> extend the current prefix
+        if s[i] == s[j]:
             j += 1
 
-        # store the length of the current match
-        prefix[i] = j
+        pi[i] = j
 
-    length = prefix[-1]
+    return pi
 
-    # returns prefix as a string (or empty string "" if none)
-    return word[:length]
+
+def longest_proper_prefix(s):
+    """Returns the longest proper prefix of a string that is also its suffix."""
+
+    pi = prefix_func(s)
+    length = pi[-1] 
+
+    if length == 0:
+        return ""
+    else:
+        return s[:length]
 
 print(longest_proper_prefix("baobab"))   # must return b
 print(longest_proper_prefix("azazaz"))  # must return azaz
